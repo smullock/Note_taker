0d2de037220bfab5a9d2edf8b80c0499b8ed1ec0
Sarah Mullock <smullock@hotmail.com>
2023-02-12 16:29:40 +1100

    change html route

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Initialize our app variable by setting it to the value of express()
 const app = express();
-const PORT = process.env.PORT ||5001;
+const PORT = process.env.PORT ||3000;
 const fs = require("fs");
 const data = require("./db/db.json")
 var uniqid = require('uniqid');
@@ -19,18 +19,15 @@ app.use(express.json());
 
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, '/index.html'))
+  res.sendFile(path.join(__dirname, "../public/index.html"))
 );
 
 //return the notes.html file
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/notes.html'))
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
 );
 
-//return the index.html file
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
+
 
 
 //GET /api/notes should read the db.json file and return all saved notes as JSON.
@@ -45,7 +42,7 @@ app.get('/api/notes', (req, res) => {
   })
 });
 
-
+//POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 app.post('/api/notes', (req, res) => {
   console.log(`${req.method} request received to add a note`)
@@ -82,8 +79,11 @@ app.post('/api/notes', (req, res) => {
       res.json('unable to post note')
   }
 })
-//POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 
-//bonus
-//DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file./*  */
+//return the index.html file
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
+
